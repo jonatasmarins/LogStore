0958e7f74e69078d0057adc37e690aaeca53b428
jonatasmarins <jonatasmarins_leite@hotmail.com>
2020-08-17 16:03:09 -0300

    ajustando teste unitario

--- LogStore.Domain/obj/Debug/netcoreapp3.1/Logstore.Domain.AssemblyInfo.cs
+++ LogStore.Domain/obj/Debug/netcoreapp3.1/Logstore.Domain.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LogStore.Domain")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Logstore.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("LogStore.Domain")]
-[assembly: System.Reflection.AssemblyTitleAttribute("LogStore.Domain")]
+[assembly: System.Reflection.AssemblyProductAttribute("Logstore.Domain")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Logstore.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
